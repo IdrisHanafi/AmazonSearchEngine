397eceac57a9c8fc9f6e062b737e08c423d848c3
Idris <ihanafi@umich.edu>
2022-07-10 15:47:40 +0000

    Updated DVC Setup

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
-# 697-Capstone
+# 697 Capstone: Amazon Product Search Engine 
+
+## Overview
+
+This is the repository that includes the code, data, and model components for the engine.
+
+## Prerequisites
+
+You will need the following things properly installed on your computer.
+
+* [Git](https://git-scm.com/downloads)
+* [Python3.6+](https://www.python.org/downloads/)
+* [Conda](https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html)
+
+### 1. Environment Setup
 
 To get started, setup the environment:
 
 ```
 $ conda env create -f environment.yml
 $ conda activate 697-env
+```
+
+### 2. Reproduce the output
+
+Afterwards, you can reproduce the pipeline by running the following:
+
+```
+$ dvc repro
 ```
--- src/data_clean.py
+++ src/data_clean.py
@@ -1,15 +1,54 @@
 import boto3
 import pandas as pd
+from argparse import ArgumentParser, Namespace
 
 def load_data_from_s3(bucket, file_name_and_path):
-    data_location = 's3://{}/{}'.format(bucket, data_key)
+    data_location = 's3://{}/{}'.format(bucket, file_name_and_path)
     df = pd.read_csv(data_location)
     
     return df
 
-if __name__ == "__main__":
-    bucket = '697-datasets' # Or whatever you called your bucket
-    data_key = 'meta_Electronics-0.1-percent.csv' # Where the file is within your bucket
+def clean_data(df):
+    """
+    Parses the needed features and explodes columns
+    """
     
-    df = load_data_from_s3(bucket, data_key)
+    
+    return df
+
+def parse_args() -> Namespace:
+    """parse arguments"""
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--input_bucket",
+        help="The S3 Bucket that contains input files to read from",
+        type=str
+    )
+    parser.add_argument(
+        "--input_file",
+        help="The Input S3 File Directory that contains csv files to read from",
+        type=str
+    )
+    parser.add_argument(
+        "--output_file",
+        help="The output file",
+        type=str
+    )
+    
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    
+    parsed_args = parse_args()
+
+    bucket = parsed_args.input_bucket
+    input_file = parsed_args.input_file
+    output_file = parsed_args.output_file
+    
+    print(bucket, input_file)
+    
+    df = load_data_from_s3(bucket, input_file)
+    df = clean_data(df)
     print(df.head())
+    
+    df.to_csv(output_file)
