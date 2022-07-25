16faf878049c2ca0f8479874379d2753e56bc4b7
Ubuntu <ubuntu@ip-172-31-19-147.ec2.internal>
2022-07-25 21:53:53 +0000

    Download data script

---
+++ get_data_and_model.sh
@@ -0,0 +1,7 @@
+curl -o datasets.zip https://697-public-datasets.s3.us-east-2.amazonaws.com/datasets.zip
+curl -o index.zip https://697-public-datasets.s3.us-east-2.amazonaws.com/index.zip
+curl -o models.zip https://697-public-datasets.s3.us-east-2.amazonaws.com/models.zip
+
+unzip datasets.zip
+unzip index.zip
+unzip models.zip
