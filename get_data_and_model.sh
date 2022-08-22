610cedf9c7e8eadae5559999ff455f4a26a3617f
idrishanafi <ifh101@gmail.com>
2022-08-21 23:52:32 -0400

    Updated data pull

--- get_data_and_model.sh
+++ get_data_and_model.sh
@@ -2,10 +2,12 @@ curl -o datasets.zip https://697-public-datasets.s3.us-east-2.amazonaws.com/data
 curl -o index.zip https://697-public-datasets.s3.us-east-2.amazonaws.com/index.zip
 curl -o models.zip https://697-public-datasets.s3.us-east-2.amazonaws.com/models.zip
 curl -o meta_Electronics.json.gz https://697-public-datasets.s3.us-east-2.amazonaws.com/meta_Electronics.json.gz
+curl -o Electronics_5.json https://697-public-datasets.s3.us-east-2.amazonaws.com/Electronics.json
 curl -o R1_data_indexed_lines.json https://697-public-datasets.s3.us-east-2.amazonaws.com/R1_data_indexed_lines.json
 
 unzip datasets.zip
 unzip index.zip
 unzip models.zip
 mv meta_Electronics.json.gz datasets/ && cd datasets/ && gzip -d meta_Electronics.json.gz
+mv Electronics_5.json datasets/
 mkdir datasets/r1_data/ && mv R1_data_indexed_lines.json datasets/r1_data/
